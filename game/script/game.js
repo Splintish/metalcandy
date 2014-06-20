e74c0abdc0ba26a16ce9948ce46e3ad2030f16e5
Splintish <splintish@craftsector.net>
2014-06-20 02:03:18 -0700

    experimenting with physics
    Added favicon to website. Experimented with p2.js physics in Phaser.

--- game/script/game.js
+++ game/script/game.js
@@ -1,12 +1,20 @@
 var game = new Phaser.Game(430, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
-game.load.image('coin', '../image/coin.png');
+    game.load.image('coin', 'game/image/coin.png');
 }
     
 function create() {
-game.add.sprite(0, 0, 'coin');
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.gravity.y = 500;
+    game.input.onDown.add(addRemove, this);
 }
+
+function addRemove(pointer){
+    var coin = game.add.sprite(pointer.position.x, pointer.position.y, "coin");
+    game.physics.p2.enable(coin);
+    coin.body.setCircle(7)
+    }
 
 function update() {
 }
